84678c0267a3938d960a49e6a8324040c25b35f5
Miguel R <m1gu3ldara20@gmail.com>
2023-08-01 22:08:40 -0500

    Add and Subtraction Function
    Create a function to add and subtract 2 integer variables called num1 and num2

--- main/calculator.py
+++ main/calculator.py
@@ -1,1 +1,16 @@
 # Calculator pro 1.0
+
+# Function to add
+def fun_add(num1, num2):
+    return num1 + num2
+
+# Function for subtraction
+def fun_subtraction(num1, num2):
+    return num1 - num2
+
+num1 = 2
+num2 = 3
+
+print(fun_add(num1, num2))
+
+print(fun_subtraction(num1, num2))
